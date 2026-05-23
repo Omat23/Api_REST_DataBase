db124c5294c47d444b439224100b5fb33ea66f0c
OmarGonzalez <omendezcarreon@gmail.com>
2026-05-22 23:01:06 -0600

    feat(model-user): add jpa annotations in user model

--- ApiRest/src/main/java/com/example/Api/ApiRest/Models/User.java
+++ ApiRest/src/main/java/com/example/Api/ApiRest/Models/User.java
@@ -1,18 +1,28 @@
 package com.example.Api.ApiRest.Models;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity //Indicates that this class is an entity and will be mapped to a database table
+@Table(name = "users") //Specifies the name of the database table to be used for mapping
 @Getter //Create the getters of the model
 @NoArgsConstructor //Create an empty constructor
 public class User {
 
     //User attributes
+    @Id //Indicates that this field is the primary key of the entity
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Specifies that the primary key will be generated automatically by the database
     private int id;
+    @Column(name = "name", nullable = false) //Specifies the name of the column in the database and that it cannot be null
     private String name;
+    @Column(name = "surname", nullable = false) //Specifies the name of the column in the database and that it cannot be null
     private String surname;
+    @Column(name = "age", nullable = false) //Specifies the name of the column in the database and that it cannot be null
     private int age;
+    @Column(name = "email", nullable = false) //Specifies the name of the column in the database and that it cannot be null
     private String email;
+    @Column(name = "password", nullable = false) //Specifies the name of the column in the database and that it cannot be null
     private String password;
 
     //It is required to avoid entering a manual ID
