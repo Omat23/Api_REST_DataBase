a11412176a13e886f1d342905ae44ff31cab5a60
Omar Gonzalez <omendezcarreon@gmail.com>
2026-06-04 11:21:47 -0600

    bugfix(mapper): fix @Mapper annotation to register as bean

--- ApiRest/src/main/java/com/example/Api/ApiRest/Mapping/UserMapper.java
+++ ApiRest/src/main/java/com/example/Api/ApiRest/Mapping/UserMapper.java
@@ -4,7 +4,7 @@ import com.example.Api.ApiRest.DTOs.UserDTO;
 import com.example.Api.ApiRest.Models.User;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
     User toUser(UserDTO userDTO);
