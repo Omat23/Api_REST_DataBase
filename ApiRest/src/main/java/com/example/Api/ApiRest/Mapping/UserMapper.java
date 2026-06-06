97dbe258a8a66282913f8cee68943c948129f830
Omar Gonzalez <omendezcarreon@gmail.com>
2026-06-05 23:00:17 -0600

    feat(mapper): add mapper to user_response_dto

--- ApiRest/src/main/java/com/example/Api/ApiRest/Mapping/UserMapper.java
+++ ApiRest/src/main/java/com/example/Api/ApiRest/Mapping/UserMapper.java
@@ -1,12 +1,15 @@
 package com.example.Api.ApiRest.Mapping;
 
-import com.example.Api.ApiRest.DTOs.UserDTO;
+import com.example.Api.ApiRest.DTOs.UserRequestDTO;
+import com.example.Api.ApiRest.DTOs.UserResponseDTO;
 import com.example.Api.ApiRest.Models.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    User toUser(UserDTO userDTO);
+    User toUser(UserRequestDTO userDTO);
+
+    UserResponseDTO toResponseDTO(User user);
 
 }
