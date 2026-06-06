29832d9a2aeb8b7a08aca7ad51687fd25e0c9209
Omar Gonzalez <omendezcarreon@gmail.com>
2026-06-05 23:11:31 -0600

    feat(controller): implement return of user_response_dto in response_entity to post endpoint /add

--- ApiRest/src/main/java/com/example/Api/ApiRest/Controller/UserController.java
+++ ApiRest/src/main/java/com/example/Api/ApiRest/Controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.Api.ApiRest.Controller;
 
-import com.example.Api.ApiRest.DTOs.UserDTO;
+import com.example.Api.ApiRest.DTOs.UserRequestDTO;
+import com.example.Api.ApiRest.DTOs.UserResponseDTO;
 import com.example.Api.ApiRest.Service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
 
     private UserService userService;
 
     @PostMapping("/add")
-    public String addUser(
+    public ResponseEntity<UserResponseDTO> addUser(
             @RequestBody
-            UserDTO userDTO
+            UserRequestDTO userRequestDTO
             ){
 
-        userService.addUser(userDTO);
-        return "User added successfully";
+        UserResponseDTO userResponseDTO = userService.addUser(userRequestDTO);
+        return new ResponseEntity<>(userResponseDTO, HttpStatus.CREATED);
     }
-
 }
