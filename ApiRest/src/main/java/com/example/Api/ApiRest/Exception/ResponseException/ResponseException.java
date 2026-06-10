66de8bf6252097e72696f87269c67e5b02fb90d4
Omar Gonzalez <omendezcarreon@gmail.com>
2026-06-10 04:14:59 -0600

    feat(exception): delete field message and add @data annotation in response-exception

--- ApiRest/src/main/java/com/example/Api/ApiRest/Exception/ResponseException/ResponseException.java
+++ ApiRest/src/main/java/com/example/Api/ApiRest/Exception/ResponseException/ResponseException.java
@@ -1,16 +1,17 @@
 package com.example.Api.ApiRest.Exception.ResponseException;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class ResponseException<T>{
 
     private String title;
-    private String message;
     private T content;
     private int codeError;
     private LocalDateTime timestamp;
