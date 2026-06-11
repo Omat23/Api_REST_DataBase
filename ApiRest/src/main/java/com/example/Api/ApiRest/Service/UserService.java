812a9038e99fb477e2cb71a867a477c1dd867e6e
Omar Gonzalez <omendezcarreon@gmail.com>
2026-06-11 03:20:04 -0600

    feature(service): implement bcrypt to encrypt the password of user_request_dto

--- ApiRest/src/main/java/com/example/Api/ApiRest/Service/UserService.java
+++ ApiRest/src/main/java/com/example/Api/ApiRest/Service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.Api.ApiRest.Service;
 
+import com.example.Api.ApiRest.Config.SecurityConfig;
 import com.example.Api.ApiRest.DTOs.UserRequestDTO;
 import com.example.Api.ApiRest.DTOs.UserResponseDTO;
 import com.example.Api.ApiRest.Mapping.UserMapper;
@@ -16,8 +17,16 @@ public class UserService {
 
     private UserMapper userMapper;
 
+    private SecurityConfig securityConfig;
+
     public UserResponseDTO addUser(UserRequestDTO userRequestDTO){
+        userRequestDTO.setPassword(encryptPassword(userRequestDTO.getPassword()));
         User user = userMapper.toUser(userRequestDTO);
         return userMapper.toResponseDTO(userRepository.save(user));
+    }
+
+    //add method to update user and encrypt password
+    private String encryptPassword(String password){
+        return securityConfig.bCrypt().encode(password);
     }
 }
