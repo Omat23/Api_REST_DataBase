c5f72a425599e9bd03abf13137cef43c746cbf67
Omar Gonzalez <omendezcarreon@gmail.com>
2026-06-12 14:21:49 -0600

    feat(service): add logic of service to get an user

--- ApiRest/src/main/java/com/example/Api/ApiRest/Service/UserService.java
+++ ApiRest/src/main/java/com/example/Api/ApiRest/Service/UserService.java
@@ -3,11 +3,18 @@ package com.example.Api.ApiRest.Service;
 import com.example.Api.ApiRest.Config.SecurityConfig;
 import com.example.Api.ApiRest.DTOs.UserRequestDTO;
 import com.example.Api.ApiRest.DTOs.UserResponseDTO;
+import com.example.Api.ApiRest.Exception.Exceptions.BadCredentialsException;
+import com.example.Api.ApiRest.Exception.Exceptions.EmailNotFoundException;
+import com.example.Api.ApiRest.Exception.Exceptions.ResourceNotFoundException;
 import com.example.Api.ApiRest.Mapping.UserMapper;
 import com.example.Api.ApiRest.Models.User;
 import com.example.Api.ApiRest.Repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -28,5 +35,15 @@ public class UserService {
     //add method to update user and encrypt password
     private String encryptPassword(String password){
         return securityConfig.bCrypt().encode(password);
+    }
+
+    public UserResponseDTO getUser(String email, String password){
+
+        User userResponse = userRepository.findByEmail(email).orElseThrow(EmailNotFoundException::new);
+
+        if(!securityConfig.bCrypt().matches(password, userResponse.getPassword())){
+            throw new BadCredentialsException();
+        }
+        return userMapper.toResponseDTO(userResponse);
     }
 }
