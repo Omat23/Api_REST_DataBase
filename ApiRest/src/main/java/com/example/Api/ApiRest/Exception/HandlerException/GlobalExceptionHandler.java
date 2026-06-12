6e8082ed250b35ab1a439e1703aa5549b0c041e2
Omar Gonzalez <omendezcarreon@gmail.com>
2026-06-12 14:20:19 -0600

    feat(global-exception-handler): add handler to resource-not-found-exception and authentication exception

--- ApiRest/src/main/java/com/example/Api/ApiRest/Exception/HandlerException/GlobalExceptionHandler.java
+++ ApiRest/src/main/java/com/example/Api/ApiRest/Exception/HandlerException/GlobalExceptionHandler.java
@@ -1,6 +1,12 @@
 package com.example.Api.ApiRest.Exception.HandlerException;
 
+import com.example.Api.ApiRest.Exception.Exceptions.BadCredentialsException;
+import com.example.Api.ApiRest.Exception.Exceptions.BaseAppException;
+import com.example.Api.ApiRest.Exception.Exceptions.EmailNotFoundException;
+import com.example.Api.ApiRest.Exception.Exceptions.ResourceNotFoundException;
+import com.example.Api.ApiRest.Exception.HandlerException.Service.ExceptionService;
 import com.example.Api.ApiRest.Exception.ResponseException.ResponseException;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,7 +18,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@AllArgsConstructor
 public class GlobalExceptionHandler {
+
+    private ExceptionService exceptionService;
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseException<Map<String, String>>> handlerMethodArgumentNotValidException(MethodArgumentNotValidException validException){
@@ -28,4 +37,18 @@ public class GlobalExceptionHandler {
         ), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ResponseException<Map<String, String>>> handlerResourceNotFoundException(ResourceNotFoundException resourceNotFoundException) {
+        return new ResponseEntity<>(new ResponseException<>(
+                resourceNotFoundException.getMessage(),
+                resourceNotFoundException.getResources(),
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now()
+        ), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({EmailNotFoundException.class, BadCredentialsException.class})
+    public ResponseEntity<ResponseException<Map<String, String>>> handlerAuthenticationException(){
+        return exceptionService.buildUnauthorizedResponse();
+    }
 }
